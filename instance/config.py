73127230d15eb42604fedb25d8ead142395505fc
Diego Olinek <164029535+diegoolinek@users.noreply.github.com>
2025-08-04 14:07:47 -0300

    config.py

--- instance/config.py
+++ instance/config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
